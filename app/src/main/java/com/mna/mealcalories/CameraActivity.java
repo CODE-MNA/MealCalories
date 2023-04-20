233700c8779cf59bd09874cfe0e58ab2e1998111
Eunah Kim <kasmisowoo@naver.com>
2023-04-20 13:31:19 -0400

    Noman camera is not working!! please fix it

--- app/src/main/java/com/mna/mealcalories/CameraActivity.java
+++ app/src/main/java/com/mna/mealcalories/CameraActivity.java
@@ -1,52 +1,65 @@
-
 package com.mna.mealcalories;
 
+        import androidx.activity.result.ActivityResult;
+        import androidx.activity.result.ActivityResultCallback;
+        import androidx.activity.result.ActivityResultLauncher;
+        import androidx.activity.result.contract.ActivityResultContracts;
         import androidx.appcompat.app.AppCompatActivity;
         import androidx.core.content.FileProvider;
-
         import android.content.Intent;
+        import android.graphics.Bitmap;
         import android.net.Uri;
         import android.os.Bundle;
         import android.os.Environment;
         import android.provider.MediaStore;
         import android.view.View;
+        import android.widget.Button;
         import android.widget.ImageView;
-
         import java.io.File;
+        import java.lang.ref.WeakReference;
 
 public class CameraActivity extends AppCompatActivity {
 
-    private ImageView imageView;
     private File file;
+
+    private static final int REQUEST_CODE = 22;
+    Button btnpicture;
+    ImageView imageView;
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-
-        // camera
         File sdcard = Environment.getExternalStorageDirectory();
         String imageFileName = "capture.jpg";
         file = new File(sdcard, imageFileName);
 
-        imageView = findViewById(R.id.imageView);
-
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        btnpicture = findViewById(R.id.btncamera_id);
+        imageView = findViewById(R.id.image);
+        btnpicture.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                capture();
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultLauncher.launch(cameraIntent);
             }
         });
-    }
 
-    private void capture() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Uri uri = Uri.fromFile(file);
-        Uri uri = FileProvider.getUriForFile(getBaseContext(), "com.onedelay.chap7.fileprovider", file);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
 
-        // resolveActivity 메서드를 이용해 카메라 앱이 있는지 확인 가능 (하지만 이 메서드는 없었다고 한다)
-        startActivityForResult(intent, 101);
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                Bundle extras = result.getData().getExtras();
+                Uri imageUri;
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                WeakReference<Bitmap> result_1 = new WeakReference<>(Bitmap.createScaledBitmap(imageBitmap,
+                                imageBitmap.getWidth(), imageBitmap.getHeight(), false).
+                        copy(Bitmap.Config.RGB_565, true));
+                Bitmap bm = result_1.get();
+//                imageUri = saveImage(bm, CameraActivity.this);
+//                imageView.setImageURI(imageUri);
+            }
+        });
     }
 }
